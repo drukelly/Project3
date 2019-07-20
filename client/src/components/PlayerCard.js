a5bb59dd1903fa135f92018804d19f85072490a0
aljoker2001 <aljoker2001@hotmail.com>
2019-07-19 19:22:47 -0700

    Added code to redirect to specific player card when the image is clicked

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class PlayerCard extends React.Component {
     state = {
-        players: []
+        players: [],
+        link: ''
     }
     componentDidMount() {
-        
         fetch('/api/team')
             .then(results => results.json())
             .then(res => {
@@ -14,28 +15,33 @@ class PlayerCard extends React.Component {
                 console.log('state players', this.state.players)
             })
     }
+    showCardDetail = (event) => {
+        console.log(event.target.id)
+        this.setState({link: '/players/' + event.target.id})
+        console.log(this.state.link)
+    }
     render() {
         return (
             <table>
                 <thead>
-                <tr>
-                    <th></th>
-                    <th>Name</th>
-                    <th>Jersey Number</th>
-                    <th>Position</th>
-                    <th>Games Played</th>
-                </tr>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Jersey Number</th>
+                        <th>Position</th>
+                        <th>Games Played</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {this.state.players.map((player) => (
-                    <tr key={player.id} id={player.id}>
-                    <td><img src={player.image}/></td>
-                    <td>{player.name}</td>
-                    <td>{player.jersey_number}</td>
-                    <td>{player.position}</td>
-                    <td>{player.games_played}</td>
-                    </tr>
-                ))}
+                    {this.state.players.map((player) => (
+                        <tr key={player.id} id={player.id}>
+                                <td><Link to={'/players/' + player.id}><img src={player.image} id={player.id} alt={player.name}/></Link></td>
+                                <td>{player.name}</td>
+                                <td>{player.jersey_number}</td>
+                                <td>{player.position}</td>
+                                <td>{player.games_played}</td>
+                            </tr>
+                    ))}
                 </tbody>
             </table>
         )
