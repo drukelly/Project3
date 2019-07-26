f5529f1b5fb959fa5b0400ffb26b51677e200e1f
aljoker2001 <aljoker2001@hotmail.com>
2019-07-25 21:00:23 -0700

    player cards now show stats for their respective sports

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -19,39 +19,49 @@ padding-bottom: 100px;
 `
 
 class PlayerCard extends Component {
-    state = {
-        players: []
-    }
-    componentDidMount() {
-      console.log('player card props', this.props)
-        fetch(`/api/team/${this.props.sport}`)
-            .then(results => results.json())
-            .then(res => {
-                console.log(res)
-                this.setState({ players: res })
-                console.log('state players', this.state.players)
-                console.log(this.state.players)
-            })
-            .catch(err => console.log(err))
-    }
-    render() {
-        return (
-          <Wrapper>
-            {this.state.players.map((player) => (
-              <Link to={`/players/${player.id}`} key={player.id} className='link near-black'>
-                <div className='b--black-10 bb flex items-center w-100'>
-                  <Image style={{backgroundImage: `url(${player.image})`}} id={player.id} alt={player.name} />
-                  <div className='f6 lh-copy pv2'>
-                    <div>#{player.jersey_number} {player.position}</div>
-                    <div className='b'>{player.name}</div>
-                    <div>{player.position === 'Pitcher' ? `ERA: ${player.era} W: ${player.wins} - L: ${player.losses}` : `BA: ${player.batting_average}`}</div>
+  state = {
+    players: []
+  }
+  componentDidMount() {
+    console.log('player card props', this.props)
+    fetch(`/api/team/${this.props.sport}`)
+      .then(results => results.json())
+      .then(res => {
+        console.log(res)
+        this.setState({ players: res })
+        console.log('state players', this.state.players)
+        console.log(this.state.players)
+      })
+      .catch(err => console.log(err))
+  }
+  render() {
+    return (
+      <Wrapper>
+        {this.state.players.map((player) => (
+          <Link to={`/players/${player.id}`} key={player.id} className='link near-black'>
+            <div className='b--black-10 bb flex items-center w-100'>
+              <Image style={{ backgroundImage: `url(${player.image})` }} id={player.id} alt={player.name} />
+              <div className='f6 lh-copy pv2'>
+                <div>#{player.jersey_number} {player.position}</div>
+                <div className='b'>{player.name}</div>
+                <div>
+                  {this.props.sport === 'baseball' && player.position === 'Pitcher'
+                    ? `ERA: ${player.era} W: ${player.wins} - L: ${player.losses}`
+                    : (this.props.sport === 'baseball'
+                    ? `BA: ${player.batting_average}`
+                    : (this.props.sport === 'basketball'
+                      ? `Points: ${player.pts}`
+                      : `Games Played: ${player.games_played}`
+                    ))
+                  }
                   </div>
-                </div>
-              </Link>
-            ))}
-          </Wrapper>
-        )
-    }
+              </div>
+            </div>
+          </Link>
+        ))}
+      </Wrapper>
+    )
+  }
 }
 
 export default PlayerCard
