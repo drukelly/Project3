a67d08ff359a4b39b606d9362e074ce86c55ce94
Dru Kelly <drukelly@gmail.com>
2019-08-01 20:59:52 -0700

    modal placement

--- client/src/components/pages/Signup.js
+++ client/src/components/pages/Signup.js
@@ -93,77 +93,79 @@ class Signup extends Component {
     }
     render() {
         return (
-            <form action='/signup' method='POST' className='pa4' style={{ paddingBottom: 100 }}>
+            <div>
                 {this.state.showDialog ? <Modal message={this.state.message} dismissModal={this.dismissModal} /> : null }
-                <div className="flex items-center justify-center pa4 bg-lightest-blue navy" style={{display: !this.state.created ? 'none' : 'run-in'}}>
-                    <svg className="w1" data-icon="info" viewBox="0 0 32 32" style={{fill:"currentcolor"}}>
-                        <title>info icon</title>
-                        <path d="M16 0 A16 16 0 0 1 16 32 A16 16 0 0 1 16 0 M19 15 L13 15 L13 26 L19 26 z M16 6 A3 3 0 0 0 16 12 A3 3 0 0 0 16 6"></path>
-                    </svg>
-                    <span className="lh-title ml3">{this.state.created}</span>
-                </div>
-                <h1 className='lh-title tc'>Create An Account</h1>
-                <div>{}</div>
-                <FormField
-                    htmlFor='name'
-                    type='text'
-                    name='name'
-                    value={this.state.name}
-                    onChange={this.handleChange} />
-                <FormField
-                    htmlFor='email'
-                    type='email'
-                    name='email'
-                    label='E-Mail Address'
-                    value={this.state.email}
-                    onChange={this.handleChange} />
-                <FormField
-                    htmlFor='phone'
-                    type='tel'
-                    name='phone'
-                    label='Phone Number'
-                    value={this.state.phone}
-                    onChange={this.handleChange} />
-                <div className='mb3'>
-                    <label className='db f6 mb2' htmlFor='username'>Username</label>
-                    <input
-                        className='b--black-20 br1 ba input-reset ph2 pv1 w-100'
+                <form action='/signup' method='POST' className='pa4' style={{ paddingBottom: 100 }}>
+                    <div className="flex items-center justify-center pa4 bg-lightest-blue navy" style={{display: !this.state.created ? 'none' : 'run-in'}}>
+                        <svg className="w1" data-icon="info" viewBox="0 0 32 32" style={{fill:"currentcolor"}}>
+                            <title>info icon</title>
+                            <path d="M16 0 A16 16 0 0 1 16 32 A16 16 0 0 1 16 0 M19 15 L13 15 L13 26 L19 26 z M16 6 A3 3 0 0 0 16 12 A3 3 0 0 0 16 6"></path>
+                        </svg>
+                        <span className="lh-title ml3">{this.state.created}</span>
+                    </div>
+                    <h1 className='lh-title tc'>Create An Account</h1>
+                    <div>{}</div>
+                    <FormField
+                        htmlFor='name'
                         type='text'
-                        name='username'
-                        value={typeof username !== undefined ? this.state.username : ''}
+                        name='name'
+                        value={this.state.name}
                         onChange={this.handleChange} />
-                </div>
-                <div className='mb3'>
-                    <label className='db f6 mb2' htmlFor='password'>Password</label>
-                    <input
-                        className='b--black-20 br1 ba input-reset ph2 pv1 w-100'
-                        type='password'
-                        name='password'
-                        placeholder='Enter at least 8 characters'
-                        onChange={this.handleChange}
-                        value={typeof password !== undefined ? this.state.password : ''}
-                        maxLength='15' />
-                </div>
-                <div className='mb3'>
-                    <label className='db f6 mb2' htmlFor='password'>Confirm Password</label>
-                    <input
-                        className='b--black-20 br1 ba input-reset ph2 pv1 w-100'
-                        type='password'
-                        name='password2'
-                        placeholder='Confirm your Password'
-                        onChange={this.handleChange}
-                        value={typeof password2 !== undefined ? this.state.password2 : ''}
-                        maxLength='15' />
-                </div>
-                <Button type='submit' onClick={this.handleSubmit}> Create Account </Button>
-                {this.state.redirectTo ? <Redirect to={{
-                    pathname: this.state.redirectTo,
-                    state: {
-                        style: 'run-in'
-                    }
-                    }} /> : ''}
-                <p className='f6 tc'>Have An Account? <a href='/login' className='blue link'>Log In</a></p>
-            </form>
+                    <FormField
+                        htmlFor='email'
+                        type='email'
+                        name='email'
+                        label='E-Mail Address'
+                        value={this.state.email}
+                        onChange={this.handleChange} />
+                    <FormField
+                        htmlFor='phone'
+                        type='tel'
+                        name='phone'
+                        label='Phone Number'
+                        value={this.state.phone}
+                        onChange={this.handleChange} />
+                    <div className='mb3'>
+                        <label className='db f6 mb2' htmlFor='username'>Username</label>
+                        <input
+                            className='b--black-20 br1 ba input-reset ph2 pv1 w-100'
+                            type='text'
+                            name='username'
+                            value={typeof username !== undefined ? this.state.username : ''}
+                            onChange={this.handleChange} />
+                    </div>
+                    <div className='mb3'>
+                        <label className='db f6 mb2' htmlFor='password'>Password</label>
+                        <input
+                            className='b--black-20 br1 ba input-reset ph2 pv1 w-100'
+                            type='password'
+                            name='password'
+                            placeholder='Enter at least 8 characters'
+                            onChange={this.handleChange}
+                            value={typeof password !== undefined ? this.state.password : ''}
+                            maxLength='15' />
+                    </div>
+                    <div className='mb3'>
+                        <label className='db f6 mb2' htmlFor='password'>Confirm Password</label>
+                        <input
+                            className='b--black-20 br1 ba input-reset ph2 pv1 w-100'
+                            type='password'
+                            name='password2'
+                            placeholder='Confirm your Password'
+                            onChange={this.handleChange}
+                            value={typeof password2 !== undefined ? this.state.password2 : ''}
+                            maxLength='15' />
+                    </div>
+                    <Button type='submit' onClick={this.handleSubmit}> Create Account </Button>
+                    {this.state.redirectTo ? <Redirect to={{
+                        pathname: this.state.redirectTo,
+                        state: {
+                            style: 'run-in'
+                        }
+                        }} /> : ''}
+                    <p className='f6 tc'>Have An Account? <a href='/login' className='blue link'>Log In</a></p>
+                </form>
+            </div>
         )
     }
 }
