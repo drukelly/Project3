8bf31a483fbafd3f7ce5af45ef874c938adbbe49
Dru Kelly <drukelly@gmail.com>
2019-08-03 18:17:22 -0700

    video bg poc

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { Link, Redirect } from 'react-router-dom'
 import Modal from './../Modal'
